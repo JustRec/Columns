844f1d7cc759e3b8a77030537813e322dd6b52d9
Syntria <egegermen2000@gmail.com>
2022-05-16 00:09:20 +0300

    High Score Table

--- ColumnsGame.java
+++ ColumnsGame.java
@@ -4,140 +4,226 @@ import enigma.console.TextAttributes;
 import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.util.Locale;
 import java.util.Random;
+import java.util.Scanner;
 
 public class ColumnsGame {
 
-    enigma.console.Console cn = Enigma.getConsole("Star Trek Warp Wars");
+	enigma.console.Console cn = Enigma.getConsole("Star Trek Warp Wars");
 
-    SingleLinkedList box = new SingleLinkedList();
+	SingleLinkedList box = new SingleLinkedList();
 
-    SingleLinkedList column1 = new SingleLinkedList();
-    SingleLinkedList column2 = new SingleLinkedList();
-    SingleLinkedList column3 = new SingleLinkedList();
-    SingleLinkedList column4 = new SingleLinkedList();
-    SingleLinkedList column5 = new SingleLinkedList();
+	SingleLinkedList column1 = new SingleLinkedList();
+	SingleLinkedList column2 = new SingleLinkedList();
+	SingleLinkedList column3 = new SingleLinkedList();
+	SingleLinkedList column4 = new SingleLinkedList();
+	SingleLinkedList column5 = new SingleLinkedList();
+	DoubleLinkedList dll_scores = new DoubleLinkedList();
+	DoubleLinkedList dll_names = new DoubleLinkedList();
+	DoubleLinkedList dll_highscore = new DoubleLinkedList();
 
-    int transferCount = 0;
-    int playerScore = 0;
-    int boxNumber = 0;
+	int transferCount = 0;
+	int playerScore = 0;
+	int boxNumber = 0;
 
+	Random rnd = new Random();
 
-    Random rnd = new Random();
+	int randomCardIndex;
 
-    int randomCardIndex;
+	int[] cards = new int[] { 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6,
+			7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 10, 10, 10, 10, 10 };
 
-    int[] cards = new int[] {1,1,1,1,1,2,2,2,2,2,3,3,3,3,3,4,4,4,4,4,5,5,5,5,5,6,6,6,6,6,7,7,7,7,7,8,8,8,8,8,9,9,9,9,9,10,10,10,10,10};
+	public void gameRun() {
 
-    public void gameRun() {
+		
+		  while (cards.length > 0){
+		  
+		  
+		  randomCardIndex = rnd.nextInt(cards.length);
+		  
+		  if (column1.size() < 6) {
+		  
+		  column1.add(cards[randomCardIndex]); }
+		  
+		  else if (column2.size() < 6) {
+		  
+		  column2.add(cards[randomCardIndex]); }
+		  
+		  else if (column3.size() < 6) {
+		  
+		  column3.add(cards[randomCardIndex]); }
+		  
+		  else if (column4.size() < 6) {
+		  
+		  column4.add(cards[randomCardIndex]); }
+		  
+		  else if (column5.size() < 6) {
+		  
+		  column5.add(cards[randomCardIndex]); }
+		  
+		  else { box.add(cards[randomCardIndex]); }
+		  
+		  
+		  
+		  cards = removeItemFromArray(cards, randomCardIndex);
+		  
+		  }
+		  
+		  
+			/*
+			 * box.printSingleLinkedList(); System.out.println();
+			 * column1.printSingleLinkedList(); System.out.println();
+			 * column2.printSingleLinkedList(); System.out.println();
+			 * column3.printSingleLinkedList(); System.out.println();
+			 * column4.printSingleLinkedList(); System.out.println();
+			 * column5.printSingleLinkedList();
+			 */
+		  
+		  
+		  
+		  printGameScreen(cn);
+		 
 
-        while (cards.length > 0){
+		reading_scores(dll_scores, "highscore.txt");
+		reading_names(dll_names, "highscore.txt");
+		DoubleLinkedListNode temp = dll_scores.head;
+		DoubleLinkedListNode temp2 = dll_names.head;
+		while (temp != null) {
+			dll_highscore.sortedAdd(temp.getData(), temp2.getData());
+			temp = temp.getNext();
+			temp2 = temp2.getNext();
+		}
+		writing(dll_highscore,"highscore.txt");
 
+	}
 
-            randomCardIndex = rnd.nextInt(cards.length);
+	private int[] removeItemFromArray(int[] array, int indexOfTheElementToBeDeleted) {
 
-            if (column1.size() < 6) {
+		int[] tempArray = new int[array.length - 1];
 
-                column1.add(cards[randomCardIndex]);
-            }
+		for (int k = 0; k < array.length; k++) {
 
-            else if (column2.size() < 6) {
+			if (k < indexOfTheElementToBeDeleted) {
+				tempArray[k] = array[k];
+			}
 
-                column2.add(cards[randomCardIndex]);
-            }
+			else if (k > indexOfTheElementToBeDeleted) {
+				tempArray[k - 1] = array[k];
+			}
+		}
 
-            else if (column3.size() < 6) {
+		array = tempArray;
 
-                column3.add(cards[randomCardIndex]);
-            }
+		return array;
+	}
 
-            else if (column4.size() < 6) {
+	private void printGameScreen(Console console) {
 
-                column4.add(cards[randomCardIndex]);
-            }
+		column1.printSingleLinkedList("C1", 7, 3);
+		column2.printSingleLinkedList("C2", 11, 3);
+		column3.printSingleLinkedList("C3", 15, 3);
+		column4.printSingleLinkedList("C4", 19, 3);
+		column5.printSingleLinkedList("C5", 23, 3);
 
-            else if (column5.size() < 6) {
+		console.getTextWindow().setCursorPosition(30, 3);
+		console.getTextWindow().output("Transfer: " + transferCount);
+		console.getTextWindow().setCursorPosition(30, 4);
+		console.getTextWindow().output("Score:    " + playerScore);
 
-                column5.add(cards[randomCardIndex]);
-            }
+		console.getTextWindow().setCursorPosition(30, 8);
+		console.getTextWindow().output("Box");
+		console.getTextWindow().setCursorPosition(30, 9);
+		console.getTextWindow().output("+--+");
+		console.getTextWindow().setCursorPosition(30, 10);
 
-            else {
-                box.add(cards[randomCardIndex]);
-            }
+		if (boxNumber != 0) {
+			console.getTextWindow().output("| " + boxNumber + "|");
+		}
 
+		else {
+			console.getTextWindow().output("|  |");
+		}
 
+		console.getTextWindow().setCursorPosition(30, 11);
+		console.getTextWindow().output("+--+");
 
-            cards = removeItemFromArray(cards, randomCardIndex);
+	}
 
-        }
+	public DoubleLinkedList reading_names(DoubleLinkedList dll, String string) {
+		try {
+			File file = new File(string);
+			Scanner scan = new Scanner(file);
 
-        /* box.printSingleLinkedList();
-        System.out.println();
-        column1.printSingleLinkedList();
-        System.out.println();
-        column2.printSingleLinkedList();
-        System.out.println();
-        column3.printSingleLinkedList();
-        System.out.println();
-        column4.printSingleLinkedList();
-        System.out.println();
-        column5.printSingleLinkedList(); */
+			while (scan.hasNextLine()) {
+				String line = scan.nextLine();
+				line = line.toUpperCase(Locale.ENGLISH);
+				int space = (line.indexOf(" "));
+				dll.add(line.substring(0, space));
+			}
+			scan.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
 
-        printGameScreen(cn);
+		return dll;
+	}
 
-    }
+	public DoubleLinkedList reading_scores(DoubleLinkedList dll, String string) {
 
-    private int[] removeItemFromArray (int[] array, int indexOfTheElementToBeDeleted) {
+		try {
+			File file = new File(string);
+			Scanner scan = new Scanner(file);
 
-        int[] tempArray = new int[array.length-1];
+			while (scan.hasNextLine()) {
+				String line = scan.nextLine();
+				line = line.toUpperCase(Locale.ENGLISH);
+				int space = (line.indexOf(" "));
+				dll.add(Integer.parseInt(line.substring(space + 1)));
+			}
+			scan.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
 
-        for (int k = 0; k < array.length; k++) {
+		return dll;
+	}
+	public void writing(DoubleLinkedList dll, String string) {
+		File file = new File(string);
+		try {
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+			FileWriter fw = new FileWriter(file, false);
+			BufferedWriter bw = new BufferedWriter(fw);
 
-            if (k < indexOfTheElementToBeDeleted) {
-                tempArray[k] = array[k];
-            }
+			DoubleLinkedListNode temp = dll.tail;
+			DoubleLinkedListNode temp2 = dll.tail2;
+			int count = 0;
+			while (count < dll.size()) {
 
-            else if (k > indexOfTheElementToBeDeleted) {
-                tempArray[k - 1] = array[k];
-            }
-        }
+				String table_score = String.valueOf(temp.getData());
+				temp = temp.getPrev();
+				String table_name = temp2.getData().toString();
+				temp2 = temp2.getPrev();
 
-        array = tempArray;
+				bw.write(table_name + " " + table_score + "\n");
+				count++;
 
-        return array;
-    }
+			}
+			bw.close();
 
-    private void printGameScreen(Console console) {
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 
-        column1.printSingleLinkedList("C1", 7,3);
-        column2.printSingleLinkedList("C2", 11,3);
-        column3.printSingleLinkedList("C3", 15,3);
-        column4.printSingleLinkedList("C4", 19,3);
-        column5.printSingleLinkedList("C5", 23,3);
+	}
 
-        console.getTextWindow().setCursorPosition(30,3);
-        console.getTextWindow().output("Transfer: " + transferCount);
-        console.getTextWindow().setCursorPosition(30,4);
-        console.getTextWindow().output("Score:    " + playerScore);
-
-
-        console.getTextWindow().setCursorPosition(30,8);
-        console.getTextWindow().output("Box");
-        console.getTextWindow().setCursorPosition(30,9);
-        console.getTextWindow().output("+--+");
-        console.getTextWindow().setCursorPosition(30,10);
-
-        if (boxNumber != 0) {
-            console.getTextWindow().output("| " + boxNumber + "|");
-        }
-
-        else {
-            console.getTextWindow().output("|  |");
-        }
-
-        console.getTextWindow().setCursorPosition(30,11);
-        console.getTextWindow().output("+--+");
-
-
-    }
+	
 }
---
+++ DoubleLinkedList.java
@@ -0,0 +1,207 @@
+public class DoubleLinkedList {
+	DoubleLinkedListNode head;
+	DoubleLinkedListNode tail;
+	DoubleLinkedListNode head2;
+	DoubleLinkedListNode tail2;
+	
+	public DoubleLinkedList() {
+		head = null;
+		tail = null;
+		head2 = null;
+		tail2 = null;
+	}
+	
+	public void add(Object data) {
+		DoubleLinkedListNode newNode = new DoubleLinkedListNode(data);
+		if(head == null) {
+			head = newNode;
+			tail = newNode;
+		}
+		else {
+			newNode.setPrev(tail);
+			tail.setNext(newNode);
+			tail = newNode;
+		}
+	}
+	
+	public void sortedAdd(Object dataToAdd,Object dataToAdd2) {
+
+        if(head==null) 
+        {
+        	DoubleLinkedListNode newNode=new DoubleLinkedListNode(dataToAdd);
+            head=newNode;
+            tail=newNode;
+            DoubleLinkedListNode newNode2=new DoubleLinkedListNode(dataToAdd2);
+            head2=newNode2;
+            tail2=newNode2;
+        }
+        
+        else 
+        {
+        	DoubleLinkedListNode newNode=new DoubleLinkedListNode(dataToAdd);
+        	DoubleLinkedListNode newNode2=new DoubleLinkedListNode(dataToAdd2);
+        	if((int)dataToAdd < (int)head.getData())  
+        	{       	
+            newNode.setNext(head);
+            head.setPrev(newNode);
+            head=newNode;
+            newNode2.setNext(head2);
+            head2.setPrev(newNode2);
+            head2=newNode2;
+        	}
+        	else 
+        	{
+        		DoubleLinkedListNode temp = head;
+        		DoubleLinkedListNode temp2 = head2;
+        		while(temp.getNext() != null && (int)dataToAdd >= (int)temp.getNext().getData()) {
+        			temp=temp.getNext();
+        			temp2=temp2.getNext();
+        		}
+        		newNode.setPrev(temp);
+        		newNode.setNext(temp.getNext());
+        		newNode2.setPrev(temp2);
+        		newNode2.setNext(temp2.getNext());
+        		if(temp.getNext() != null) {
+        			temp.getNext().setPrev(newNode);
+        			temp2.getNext().setPrev(newNode2);
+        		}
+        		else {
+        			tail=newNode;
+        			tail2=newNode2;
+        		}
+        		temp.setNext(newNode);
+        		temp2.setNext(newNode2);
+        	}   	      	
+        } 
+	}
+	
+	public int size() {
+		int count = 0;
+		if(head == null)
+			System.out.println("List is empty");
+		else {
+			DoubleLinkedListNode temp = head;
+			while(temp != null) {
+				count++;
+				temp = temp.getNext();
+			}
+		}
+		return count;
+	}
+	
+	public boolean search(int number) {
+		if(head == null)
+		{
+			System.out.println("List is empty");
+		}
+		else {
+			DoubleLinkedListNode temp = tail;
+			while(temp != null) {
+				if((int)temp.getData() == number) {
+					return true;
+				}
+				temp = temp.getPrev();
+			}
+		}
+		return false;
+	}
+	
+	public void display1() {
+		if(head == null)
+			System.out.println("List is empty");
+		else {
+			DoubleLinkedListNode temp = head;
+			while(temp != null) {
+				System.out.print(temp.getData() + " ");
+				temp = temp.getNext();
+			}
+			System.out.println();
+		}
+	}
+	
+	public void display2() {
+		if(head == null)
+			System.out.println("List is empty");
+		else {
+			DoubleLinkedListNode temp = tail;
+			while(temp != null) {
+				System.out.print(temp.getData() + " ");
+				temp = temp.getPrev();
+			}
+			System.out.println();
+		}
+	}
+	
+	public void display_names() {
+		if(head2 == null)
+			System.out.println("List is empty");
+		else {
+			DoubleLinkedListNode temp2 = tail2;
+			while(temp2 != null) {
+				System.out.print(temp2.getData() + " ");
+				temp2 = temp2.getPrev();
+			}
+			System.out.println();
+		}
+	}
+	
+	public Object getElement(int x) {
+		if(head == null)
+		{
+			System.out.println("List is empty");
+			return null;
+		}
+		else if(x > size() || x < 0 ){
+			System.out.println("Index is out of range");
+			return null;
+		}
+		else {
+			DoubleLinkedListNode temp = head;
+			int count = 1;
+			while(temp != null) {
+				if(x == count)
+					return temp.getData();
+				temp = temp.getNext();
+				count++;
+			}
+			return null;
+		}
+	}
+	
+	public void swap(int x) {
+		if(head == null)
+		{
+			System.out.println("List is empty");
+		}
+		else if(x > size() || x < 0 ){
+			System.out.println("Index is out of range");
+		}
+		else {
+			int element1 = -1;
+			DoubleLinkedListNode temp1 = head;
+			int count = 1;
+			while(temp1 != null) {
+				if(count == x) {
+					element1 = (int)temp1.getData();
+					break;
+				}
+				temp1 = temp1.getNext();
+				count++;
+			}
+			int element2 = -1;
+			DoubleLinkedListNode temp2 = tail;
+			count = 1;
+			while(temp2 != null) {
+				if(count == x) {
+					element2 = (int)temp2.getData();
+					temp2.setData(element1);
+					break;
+				}
+				temp2 = temp2.getPrev();
+				count++;
+			}			
+			temp1.setData(element2);			
+		}
+	}
+
+}
